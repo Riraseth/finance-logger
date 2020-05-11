51f2b543b2fa5487c529e3304dbd2067c495238f
Łukasz Zabuski <riraseth@gmail.com>
2020-05-12 00:35:13 +0200

    arrays and objects

--- sandbox.ts
+++ sandbox.ts
@@ -17,3 +17,44 @@ const circ = (diameter: number) => {
 
 // console.log(circ('hello'));
 console.log(circ(2));
+
+// arrays
+
+let names = ['luigi', 'mario', 'yoshi'];
+
+names.push('toad');
+// names.push(3);
+// names[0] = 3'
+
+let numbers = [1, 2, 3, 4];
+
+numbers.push(25);
+// numbers.push('bob');
+// numbers[1] = ('jen')
+
+let mixed = ['ken', 4, 'ryu', 14, 63];
+
+mixed.push(42);
+mixed.push('blanca');
+mixed[0] = 3;
+
+// objects
+
+let ninja = {
+  name: 'mario',
+  belt: 'black',
+  age: 30,
+};
+
+ninja.age = 40;
+ninja.name = 'ryu';
+// ninja.age = '30';
+// ninja.skills = ['fighting', 'sneaking']
+// can't add new properties after object was defined
+
+ninja = {
+  name: 'yoshi',
+  belt: 'yellow',
+  age: 42,
+  // skills: []
+};
