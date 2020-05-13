17f726ed67711c8702ee1f6089988fdc4a4c41dd
Łukasz Zabuski <riraseth@gmail.com>
2020-05-14 01:24:37 +0200

    tsconfig

---
+++ public/dynamic-types.js
@@ -0,0 +1,18 @@
+"use strict";
+var age = 25;
+age = true;
+console.log(age);
+age = 'hello';
+console.log(age);
+age = { name: 'bob' };
+console.log(age);
+var mixed = [];
+mixed.push(5);
+mixed.push('bob');
+mixed.push(false);
+console.log(mixed);
+var ninja;
+ninja = { name: 'bob', age: 66 };
+console.log(ninja);
+ninja = { name: 25, age: 'bob' };
+console.log(ninja);
