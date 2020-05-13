55051f8c87027cf4df818495e0aa2a5a8a5f2def
Łukasz Zabuski <riraseth@gmail.com>
2020-05-14 01:17:28 +0200

    added dynamic types, removed styles

---
+++ dynamic-types.ts
@@ -0,0 +1,26 @@
+let age: any = 25;
+
+age = true;
+console.log(age);
+age = 'hello';
+console.log(age);
+age = { name: 'bob' };
+console.log(age);
+
+let mixed: any[] = [];
+
+mixed.push(5);
+mixed.push('bob');
+mixed.push(false);
+console.log(mixed);
+
+let ninja: {
+  name: any;
+  age: any;
+};
+
+ninja = { name: 'bob', age: 66 };
+console.log(ninja);
+
+ninja = { name: 25, age: 'bob' };
+console.log(ninja);
